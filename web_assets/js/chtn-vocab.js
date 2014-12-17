a69794ecccecdd4ab59dd3d41ff4aa9953b4e571
David Robbins <robbins.david@gmail.com>
2014-12-17 08:43:42 -0600

    Added initial cross filters

--- web_assets/js/chtn-vocab.js
+++ web_assets/js/chtn-vocab.js
@@ -3,6 +3,8 @@
     'use strict';
 
     var CHTN = window.CHTN = {
+
+        // Download and Parse the Vocabulary TSV file, and populate the CHTN object.
         _init: function () {
             Papa.parse("CHTN%20Vocab-Disease%20List.txt", {
                 download: true,
@@ -13,9 +15,28 @@
                     CHTN._rawVocabulary = file;
                     CHTN._parseResults = results;
                     CHTN.vocabulary = results.data;
+                    CHTN._createCrossFilters(CHTN.vocabulary);
                     $("#raw").text(JSON.stringify(results.data, null, 2));
                 }
             });
+        },
+
+
+        _createCrossFilters: function (vocabulary) {
+            var cf = CHTN.crossfilter = crossfilter(vocabulary);
+
+            CHTN.groups     = {};
+            CHTN.dimensions = {};
+
+            CHTN.dimensions.byCategory      = cf.dimension(function (d)  { return d["Category"]; });
+            CHTN.dimensions.byAnatomicSite  = cf.dimension(function (d)  { return d["Anatomic Site"]; });
+            CHTN.dimensions.bySubsite       = cf.dimension(function (d)  { return d["Subsite"]; });
+            CHTN.dimensions.byDiagnosis     = cf.dimension(function (d)  { return d["Diagnosis"]; });
+            
+            CHTN.groups.countByCategory     = CHTN.dimensions.byCategory.group().reduceCount();
+            CHTN.groups.countByAnatomicSite = CHTN.dimensions.byAnatomicSite.group().reduceCount();
+            CHTN.groups.countBySubsite      = CHTN.dimensions.bySubsite.group().reduceCount();
+            CHTN.groups.countByDiagnosis    = CHTN.dimensions.byDiagnosis.group().reduceCount();
         }
     }
 
